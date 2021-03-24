8b3fbd3cb4edb518b7985238a555399705e6cacf
Stratos Theodorou <eevoskos@gmail.com>
2021-03-24 23:23:57 +0200

    Data checks

--- ipfs.js
+++ ipfs.js
@@ -14,6 +14,14 @@ const client = axios.create({
   }
 })
 
+function isValid (hash) {
+  return _.size(hash) === 46
+}
+
+function isTelemetry (data) {
+  return _.has(data, 'ts') && _.has(data, 'values') && _.has(data, 'geohash')
+}
+
 function getRootHash () {
   return client.post(`/api/v0/files/stat?arg=${ROOT_FOLDER}`)
     .then(response => _.get(response, 'data.Hash'))
@@ -22,9 +30,42 @@ function getRootHash () {
 function getData (hash) {
   return client.post(`/api/v0/cat?arg=${hash}`)
     .then(response => response.data)
+    .then(data => {
+      // Sometimes the hashes are strings...
+      if (_.isString(data)) {
+        // Try to parse as JSON
+        logger.log('debug', `Trying to parse as JSON`)
+        try {
+          data = JSON.parse(data)
+        } catch (error) {
+          throw new Error(`Invalid data: ${data}`)
+        }
+      }
+
+      // Sometimes they don't even contain an object
+      if (!_.isObjectLike(data)) {
+        logger.log('debug', `Data not object-like`)
+        throw new Error(`Invalid data: ${data}`)
+      }
+
+      // Sometimes the hashes contain [] instead of {}...
+      if (_.isArrayLikeObject(data)) {
+        logger.log('debug', `Data is array-like`)
+        data = data[0]
+      }
+
+      // Or sometimes the object does not contain valid telemetry
+      if (!isTelemetry(data)) {
+        logger.log('debug', `Data not telemetry`)
+        throw new Error(`Data is not telemetry: ${JSON.stringify(data)}`)
+      }
+
+      // Sometimes it's correct!
+      return data
+    })
 }
 
-function cp (hash, filename = `data_${moment().format('YYYYMMDD_HHmmssSSS')}.json`) {
+function cp (hash, filename = `${moment().format('YYYYMMDD_HHmmssSSS')}.json`) {
   let source = `/ipfs/${hash}`
   let destination = `${ROOT_FOLDER}/${filename}`
   return client.post(`/api/v0/files/cp?arg=${source}&arg=${destination}`)
@@ -41,6 +82,7 @@ function publish (hash) {
 }
 
 module.exports = {
+  isValid: isValid,
   getRootHash: getRootHash,
   getData: getData,
   cp: cp,
